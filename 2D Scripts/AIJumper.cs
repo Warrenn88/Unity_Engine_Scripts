147a5041a3af364073dc63e9ab2fbd03d1a6164c
Warren Nesteruk <warrennesteruk@gmail.com>
2025-03-20 19:59:20 -0400

    Create AIJumper.cs
    2d AI Jumper enemy

---
+++ 2D Scripts/AIJumper.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIJumper : MonoBehaviour
+{
+    public float jumpForce = 8f;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+}
